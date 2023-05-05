b24283d134dafe861a09be5c1ff05ce11d6a2dae
Jordana-Nunes <53192683+Jordana-Nunes@users.noreply.github.com>
2023-05-05 09:18:43 -0300

    adicionado metodos de calculadora
    Signed-off-by: Jordana-Nunes <53192683+Jordana-Nunes@users.noreply.github.com>

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def soma (a,b):
+    c = a + b
+    return c
+
+def sub (a,b):
+    c = a - b
+    return c
+
+def multi (a,b):
+    c = a * b
+    return c
+
+def soma (a,b):
+    c = a / b
+    return c
